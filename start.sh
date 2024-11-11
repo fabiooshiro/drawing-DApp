2616766bc80b37624616b84a9998d6da0123c3a8
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2024-11-11 12:59:32 +0000

    chore: running on gitpod

---
+++ start.sh
@@ -0,0 +1,10 @@
+brunodo --http-rollups-port=5004 --http-port=8080 \
+--sqlite-file=database.sqlite3 \
+--from-l1-block=7019226 \
+--rpc-url=wss://sepolia.gateway.tenderly.co \
+--contracts-application-address=0xb1f7cb5f48eb8f4cdf620c4638fbc68afe949486 \
+--sequencer=espresso \
+--espresso-url=https://query.decaf.testnet.espresso.network \
+--from-block=0 \
+--namespace=51025 --contracts-input-box-block=6994348 --contracts-input-box-address=0x593E5BCf894D6829Dd26D0810DA7F064406aebB6 \
+-d
